2e23e4312b39d810e35ec7710291794c5ae364c5
illupovero <davideluporini1996@gmail.com>
2022-02-14 13:27:15 +0100

    added main

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+      <div class="container d-flex justify-content-center align-items-center">
+          <div class="row">
+            <div class="h1 text-uppercase">Seleziona un film che desideri vedere</div>
+          </div>
+
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name : 'Main',
+}
+</script>
+
+<style lang="scss">
+.container{
+    min-height: calc(100vh - 80px);
+}
+</style>
