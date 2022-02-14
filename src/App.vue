4f30f3fbdc64445e287b7dc2e9adc0a21bb5a263
illupovero <davideluporini1996@gmail.com>
2022-02-14 13:15:26 +0100

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-
+    <Header/>
   </div>
 </template>
 
 <script>
-
-
+// import Axios from 'axios'
+import Header from './components/Header.vue'
 export default {
+
   name: 'App',
   components: {
+    Header,
 
   }
 }
 </script>
 
 <style lang="scss">
+@import 'bootstrap';
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="text-uppercase h1">
+      boolFlix
+    </div>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped  lang="scss">
+@import 'bootstrap';
+
+</style>
